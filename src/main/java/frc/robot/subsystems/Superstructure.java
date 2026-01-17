acf2a2c845bb2ce386a7097906c8cfb8f674aced
sohnshaik <sohan.shaik@waltonrobotics.org>
2026-01-17 16:28:34 -0500

    misc spacing edits

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -11,41 +11,45 @@ import static frc.robot.Constants.RobotK.*;
 public class Superstructure {
 
     /* declare subsystems */
-        public final EventLoop stateEventLoop = new EventLoop();
-        private State m_state = State.IDLE;
+    public final EventLoop stateEventLoop = new EventLoop();
+    private State m_state = State.IDLE;
     
     
         
 
         
         
-        /* state transitions */
-        /* requests */
-    
-        /* states */
+    /* state transitions */
+    /* requests */
 
-        public final Trigger stateTrg_idle = new Trigger(stateEventLoop, (() -> m_state == State.IDLE));
-        public final Trigger stateTrg_intaking = new Trigger(stateEventLoop, (() -> m_state == State.INTAKING));
-        public final Trigger stateTrg_intook = new Trigger(stateEventLoop, (() -> m_state == State.INTOOK));
-        public final Trigger stateTrg_shootReady = new Trigger(stateEventLoop, (() -> m_state == State.SHOOT_READY));
-        public final Trigger stateTrg_shooting = new Trigger(stateEventLoop, (() -> m_state == State.SHOOTING));
-        public final Trigger stateTrg_passReady = new Trigger(stateEventLoop, (() -> m_state == State.PASS_READY));
-        public final Trigger stateTrg_passing = new Trigger(stateEventLoop, (() -> m_state == State.PASSING));
-        public final Trigger stateTrg_climbing = new Trigger(stateEventLoop, (() -> m_state == State.CLIMBING));
-        public final Trigger stateTrg_climbed = new Trigger(stateEventLoop, (() -> m_state == State.CLIMBED));
-        public final Trigger stateTrg_releasing = new Trigger(stateEventLoop, (() -> m_state == State.RELEASING));
-        public final Trigger stateTrg_released = new Trigger(stateEventLoop, (() -> m_state == State.RELEASED));
-    
-    
-        /* logs */
-    
-        private IntLogger log_stateIdx = WaltLogger.logInt(kLogTab, "state idx");
-        private StringLogger log_stateName = WaltLogger.logString(kLogTab, "state name");
+    /* states */
 
 
-        public Superstructure() {
-            
-        }
+    public final Trigger stateTrg_idle = new Trigger(stateEventLoop, (() -> m_state == State.IDLE));
+    public final Trigger stateTrg_intaking = new Trigger(stateEventLoop, (() -> m_state == State.INTAKING));
+    public final Trigger stateTrg_intook = new Trigger(stateEventLoop, (() -> m_state == State.INTOOK));
+    public final Trigger stateTrg_shootReady = new Trigger(stateEventLoop, (() -> m_state == State.SHOOT_READY));
+    public final Trigger stateTrg_shooting = new Trigger(stateEventLoop, (() -> m_state == State.SHOOTING));
+    public final Trigger stateTrg_passReady = new Trigger(stateEventLoop, (() -> m_state == State.PASS_READY));
+    public final Trigger stateTrg_passing = new Trigger(stateEventLoop, (() -> m_state == State.PASSING));
+    public final Trigger stateTrg_climbing = new Trigger(stateEventLoop, (() -> m_state == State.CLIMBING));
+    public final Trigger stateTrg_climbed = new Trigger(stateEventLoop, (() -> m_state == State.CLIMBED));
+    public final Trigger stateTrg_releasing = new Trigger(stateEventLoop, (() -> m_state == State.RELEASING));
+    public final Trigger stateTrg_released = new Trigger(stateEventLoop, (() -> m_state == State.RELEASED));
+
+
+    /* configure states/ state transitions */
+
+
+    /* logs */
+
+    private IntLogger log_stateIdx = WaltLogger.logInt(kLogTab, "state idx");
+    private StringLogger log_stateName = WaltLogger.logString(kLogTab, "state name");
+
+
+    public Superstructure() {
+
+    }
 
 
 
