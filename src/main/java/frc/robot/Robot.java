1729c6df8225db8f0b7c66f84a2b356cdf5e786a
HrehaanB <hrehaan.bhutada@gmail.com>
2026-01-18 14:20:48 -0500

    Vision/visionSim implementation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,11 +6,18 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+
 import com.ctre.phoenix6.HootAutoReplay;
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import choreo.auto.AutoFactory;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -19,14 +26,15 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.Constants.VisionK;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.vision.Vision;
+import frc.robot.vision.VisionSim;
 import frc.util.WaltLogger;
 import frc.util.WaltLogger.DoubleLogger;
 
 public class Robot extends TimedRobot {
-
-    
     private final double kMaxTranslationSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private final double kMaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
     private final double kMaxHighAngularRate = RotationsPerSecond.of(1.5).in(RadiansPerSecond);
@@ -35,14 +43,13 @@ public class Robot extends TimedRobot {
     private final DoubleLogger log_stickDesiredFieldY = WaltLogger.logDouble("Swerve", "stick desired teleop y");
     private final DoubleLogger log_stickDesiredFieldZRot = WaltLogger.logDouble("Swerve", "stick desired teleop z rot");
 
-
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+        .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
@@ -54,6 +61,13 @@ public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
     private final AutoFactory autoFactory = drivetrain.createAutoFactory();
 
+    private final VisionSim visionSim = new VisionSim();
+    private final Vision camera1 = new Vision(VisionK.kCamera1CamName, VisionK.kCamera1CamSimVisualName, VisionK.kCamera1CamRoboToCam, visionSim, VisionK.kCamera1SimProps);
+    private final Vision camera2 = new Vision(VisionK.kCamera2CamName, VisionK.kCamera2CamSimVisualName, VisionK.kCamera2CamRoboToCam, visionSim, VisionK.kCamera2SimProps);
+
+    // this should be updated with all of our cameras
+    private final Vision[] cameras = {camera1, camera2};
+
     /* log and replay timestamp and joystick data */
     private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
         .withTimestampReplay()
@@ -61,32 +75,31 @@ public class Robot extends TimedRobot {
 
     public Robot() {
         configureBindings();
-        
     }
 
-
-     private Command driveCommand() {
-    // Note that X is defined as forward according to WPILib convention,
-    // and Y is defined as to the left according to WPILib convention.
-    // Drivetrain will execute this command periodically
-    return drivetrain.applyRequest(() -> {
-      var angularRate = driver.leftTrigger().getAsBoolean() ? 
-        kMaxHighAngularRate : kMaxAngularRate;
-    
-      var driverXVelo = -driver.getLeftY() * kMaxTranslationSpeed;
-      var driverYVelo = -driver.getLeftX() * kMaxTranslationSpeed;
-      var driverYawRate = -driver.getRightX() * angularRate;
-
-      log_stickDesiredFieldX.accept(driverXVelo);
-      log_stickDesiredFieldY.accept(driverYVelo);
-      log_stickDesiredFieldZRot.accept(driverYawRate);
+    private Command driveCommand() {
+        // Note that X is defined as forward according to WPILib convention,
+        // and Y is defined as to the left according to WPILib convention.
+        // Drivetrain will execute this command periodically
+        return drivetrain.applyRequest(() -> {
+            var angularRate = driver.leftTrigger().getAsBoolean() ? 
+            kMaxHighAngularRate : kMaxAngularRate;
         
-      return drive
-        .withVelocityX(driverXVelo) // Drive forward with Y (forward)
-        .withVelocityY(driverYVelo) // Drive left with X (left)
-        .withRotationalRate(driverYawRate); // Drive counterclockwise with negative X (left)
-    });
-  }
+            var driverXVelo = -driver.getLeftY() * kMaxTranslationSpeed;
+            var driverYVelo = -driver.getLeftX() * kMaxTranslationSpeed;
+            var driverYawRate = -driver.getRightX() * angularRate;
+
+            log_stickDesiredFieldX.accept(driverXVelo);
+            log_stickDesiredFieldY.accept(driverYVelo);
+            log_stickDesiredFieldZRot.accept(driverYawRate);
+            
+            return drive
+            .withVelocityX(driverXVelo) // Drive forward with Y (forward)
+            .withVelocityY(driverYVelo) // Drive left with X (left)
+            .withRotationalRate(driverYawRate); // Drive counterclockwise with negative X (left)
+            }
+        );
+    }
 
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
@@ -126,7 +139,7 @@ public class Robot extends TimedRobot {
     }
 
     public Command getAutonomousCommand() {
-        // Simple drive forward auton
+        // Simple Auton (hardcoded)
         final var idle = new SwerveRequest.Idle();
         return Commands.sequence(
             autoFactory.resetOdometry("testAlexandra"),
@@ -134,11 +147,20 @@ public class Robot extends TimedRobot {
         );
     }
 
-
     @Override
     public void robotPeriodic() {
         m_timeAndJoystickReplay.update();
         CommandScheduler.getInstance().run(); 
+
+        for (Vision camera : cameras) {
+            Optional<EstimatedRobotPose> estimatedPoseOptional = camera.getEstimatedGlobalPose();
+            if (estimatedPoseOptional.isPresent()) {
+                EstimatedRobotPose estimatedRobotPose = estimatedPoseOptional.get();
+                Pose2d estimatedRobotPose2d = estimatedRobotPose.estimatedPose.toPose2d();
+                var ctreTime = Utils.fpgaToCurrentTime(estimatedRobotPose.timestampSeconds);
+                drivetrain.addVisionMeasurement(estimatedRobotPose2d, ctreTime, camera.getEstimationStdDevs());
+            }
+        }
     }
 
     @Override
@@ -190,5 +212,10 @@ public class Robot extends TimedRobot {
     public void testExit() {}
 
     @Override
-    public void simulationPeriodic() {}
+    public void simulationPeriodic() {
+        SwerveDriveState robotState = drivetrain.getState();
+        Pose2d robotPose = robotState.Pose;
+        visionSim.simulationPeriodic(robotPose);
+        drivetrain.simulationPeriodic();
+    }
 }
