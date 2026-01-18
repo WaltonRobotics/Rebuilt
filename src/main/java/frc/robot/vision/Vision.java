c616ccf757498d6836aee90cce6ba2e4e7c7cb77
sohnshaik <sohan.shaik@waltonrobotics.org>
2026-01-18 15:00:17 -0500

    started the painful journey of sotm

--- src/main/java/frc/robot/vision/Vision.java
+++ src/main/java/frc/robot/vision/Vision.java
@@ -40,7 +40,8 @@ public class Vision {
     private final Transform3d m_roboToCam;
 
     //Constants
-    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(1.5, 1.5, 6.24);
+    //change for Rebuilt
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(1.5, 1.5, 6.24); 
     public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 6.24);
 
     private final StructPublisher<Pose2d> log_camPose;
