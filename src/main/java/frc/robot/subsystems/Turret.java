cf821b8050a0e3878dce1079aab2e49d7e8ce053
PythonBlast7878 <promaker947@gmail.com>
2026-01-21 17:06:30 -0500

    basic turret code - UNFINISHED - move to pos, setup in constructor

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -2,33 +2,43 @@
 
 // import javax.sound.sampled.Control;
 
-// import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-// import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-// import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+// import com.ctre.phoenix6.hardware.TalonFX;
+// import com.ctre.phoenix6.configs.*;
+// import com.ctre.phoenix6.controls.*;
 
 
 // public class Turret {
-//     private final TalonSRX motor = new TalonSRX(11);
 
-//     // Constants
-//     private final double maxPos = 428;
-//     private final double minPos = -452;
-//     private final double homingPos = 342;
+//     //  ---------------- Constants --------------------
 
+    
+//     // Motor stuff
+//     private final TalonFX m_motor = new TalonFX(11);
+//     private TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+//     private final TalonFXConfigurator talonFXConfigurator = m_motor.getConfigurator();
+    
+//     // Motor Positions (for max/min assume exclusive)
+//     private final double kMaxPos = 428;
+//     private final double kMinPos = -452;
+//     private final double kHomingPos = 342;
 //     private double currentPos;
 
 //     public Turret() {
-//         currentPos = homingPos;
-
-//         motor.configAllSettings(new TalonSRXConfiguration());
-
-//         motor.moveToPos(homingPos);
+//         currentPos = kHomingPos;
+//         // TODO apply config
+//         m_motor.setPosition(currentPos);
         
 //     }
 
-//     private void moveToPos(double pos) {
-//         currentPos = motor.getSelectedSensorPosition();
+//     private boolean moveToPos(double pos) {
+//         // TODO get current pos for reasons unknown
+//         if (kMinPos < pos && kMaxPos > pos){
+//             m_motor.setPosition(pos);
+//             return true;
+//         } else {
+//             return false;
+//         }
 
-//         motor.set(TalonSRXControlMode.Position, pos);
+
 //     }
 // }
