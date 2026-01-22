74c1665a5b5f565b4888727331b5b59a4798612b
Saarth <saarth.pandya@gmail.com>
2026-01-21 22:48:10 -0500

    INCOMPLETE - added position getter and methods to move to positions

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -3,19 +3,23 @@
 // import javax.sound.sampled.Control;
 
 // import com.ctre.phoenix6.hardware.TalonFX;
+
+// import edu.wpi.first.units.AngleUnit;
+
 // import com.ctre.phoenix6.configs.*;
 // import com.ctre.phoenix6.controls.*;
+
+// import edu.wpi.first.units.*;
 
 
 // public class Turret {
 
 //     //  ---------------- Constants --------------------
-
     
 //     // Motor stuff
 //     private final TalonFX m_motor = new TalonFX(11);
-//     private TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
-//     private final TalonFXConfigurator talonFXConfigurator = m_motor.getConfigurator();
+//     // private TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+//     // private final TalonFXConfigurator talonFXConfigurator = m_motor.getConfigurator();
     
 //     // Motor Positions (for max/min assume exclusive)
 //     private final double kMaxPos = 428;
@@ -24,21 +28,41 @@
 //     private double currentPos;
 
 //     public Turret() {
-//         currentPos = kHomingPos;
 //         // TODO apply config
-//         m_motor.setPosition(currentPos);
+
+//         m_motor.getConfigurator().apply(new TalonFXConfiguration());
+
+//         currentPos = kHomingPos;
+//         m_motor.setPosition(0);
+//         moveToPos(currentPos);
         
 //     }
 
-//     private boolean moveToPos(double pos) {
+//     private double getPos() {
+//         return m_motor.getPosition().getValue().abs(Units.Rotations);
+//     }
+    
+//     public boolean moveToPos(double pos) {
 //         // TODO get current pos for reasons unknown
-//         if (kMinPos < pos && kMaxPos > pos){
-//             m_motor.setPosition(pos);
+//         if (kMinPos < pos && kMaxPos > pos) {
+//             currentPos = pos;
+//             m_motor.setPosition(currentPos);
 //             return true;
-//         } else {
+//         } 
+//         else {
 //             return false;
 //         }
+//     }
 
+//     public boolean moveToMax() {
+//         return moveToPos(kMaxPos - 0.1);
+//     }
 
+//     public boolean moveToMin() {
+//         return moveToPos(kMinPos + 0.1);
+//     }
+
+//     public boolean moveToHome() {
+//         return moveToPos(kHomingPos);
 //     }
 // }
