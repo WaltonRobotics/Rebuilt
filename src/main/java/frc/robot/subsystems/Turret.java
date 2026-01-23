e9860891180697ec0ea4d01af749a5f32d6c9115
Saarth <saarth.pandya@gmail.com>
2026-01-22 19:24:00 -0500

    calibrated to mystery unit - complete for now

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -12,14 +12,17 @@ public class Turret {
     private final TalonFX m_motor = new TalonFX(11);
     
     // Motor Positions (for max/min assume exclusive)
-    private final double kMaxPos = 428;
-    private final double kMinPos = -452;
-    private final double kHomingPos = 342;
+    private final double kMaxPos = 135;
+    private final double kMinPos = -821;
+    private final double kHomingPos = 0;
+    private final double kFrontFacingPos = -330;
+    private final double kUnitsPerRotation = 1360; // 1360 units per rotation
+
     private double currentPos;
 
     public Turret() {
         m_motor.getConfigurator().apply(new TalonFXConfiguration());
-        moveToPos(kHomingPos);
+        moveToPosRotations(kHomingPos);
     }
 
     public double getPosRotations() {
@@ -35,10 +38,10 @@ public class Turret {
      * @param pos Position in [mystery unit]
      * @return If rotation was successful
      */
-    public boolean moveToPos(double pos) {
+    public boolean moveToPosMysteryUnits(double pos) {
         if (kMinPos < pos && kMaxPos > pos) {
             currentPos = pos;
-            StatusCode code = m_motor.setControl(new MotionMagicVoltage(currentPos / 360)); // 360 is a placeholder, we dont know what unit the constants are in
+            StatusCode code = m_motor.setControl(new MotionMagicVoltage(pos / kUnitsPerRotation));
             return code == StatusCode.OK;
         } 
         else {
@@ -46,19 +49,51 @@ public class Turret {
         }
     }
 
-    public boolean moveToMax() {
-        return moveToPos(kMaxPos - 0.1);
+    /**
+     * Rotates turret to specified position.
+     * @param pos Position in rotations
+     * @return If rotation was successful
+     */
+    public boolean moveToPosRotations(double rots) {
+        if (kMinPos / kUnitsPerRotation < rots && kMaxPos / kUnitsPerRotation > rots) {
+            currentPos = rots * kUnitsPerRotation;
+            StatusCode code = m_motor.setControl(new MotionMagicVoltage(rots));
+            return code == StatusCode.OK;
+        } 
+        else {
+            return false;
+        }
     }
 
-    public boolean moveToMin() {
-        return moveToPos(kMinPos + 0.1);
+    public boolean moveToMaxMysteryUnits() {
+        return moveToPosMysteryUnits(kMaxPos - 0.1);
     }
 
-    public boolean moveToHome() {
-        return moveToPos(kHomingPos);
+    public boolean moveToMinMysteryUnits() {
+        return moveToPosMysteryUnits(kMinPos + 0.1);
     }
 
-    public boolean moveToZero() {
-        return moveToPos(0);
+    public boolean moveToHomeMysteryUnits() {
+        return moveToPosMysteryUnits(kHomingPos);
+    }
+
+    public boolean moveToFrontFacingMysteryUnits() {
+        return moveToPosMysteryUnits(0);
+    }
+
+    public boolean moveToMaxRotations() {
+        return moveToPosRotations(kMaxPos - 0.1);
+    }
+
+    public boolean moveToMinRotations() {
+        return moveToPosRotations(kMinPos + 0.1);
+    }
+
+    public boolean moveToHomeRotations() {
+        return moveToPosRotations(kHomingPos);
+    }
+
+    public boolean moveToFrontFacingRotations() {
+        return moveToPosRotations(kFrontFacingPos);
     }
 }
