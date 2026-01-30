58db81d6c1c9fe2d4da8bc1114103f9740ba32fb
alexanderan187 <alex.an@waltonrobotics.org>
2026-01-29 20:02:57 -0500

    move constants and use velocityvoltage

--- src/main/java/frc/robot/subsystems/Transfer.java
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -1,40 +1,34 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static frc.robot.Constants.TransferK.*;
+
 public class Transfer extends SubsystemBase {
-    private final TalonFX m_transferSpinnerMotor = new TalonFX(1); // change IDs
-    private final TalonFX m_transferExhaustMotor = new TalonFX(1);
+    private final TalonFX m_transferSpinnerMotor = new TalonFX(kSpinnerCANID);
+    private final TalonFX m_transferExhaustMotor = new TalonFX(kExhaustCANID);
 
     public Transfer() {
-        Slot0Configs slot0 = new Slot0Configs();
-        slot0.kS = 0.1;
-        slot0.kV = 0.12;
-        slot0.kP = 0.11;
-        slot0.kI = 0;                       // adjust values for more accuracy later
-        slot0.kD = 0;
-
-        m_transferSpinnerMotor.getConfigurator().apply(slot0);
-        m_transferExhaustMotor.getConfigurator().apply(slot0);
+        m_transferSpinnerMotor.getConfigurator().apply(kSpinnerSlot0Configs);
+        m_transferExhaustMotor.getConfigurator().apply(kExhaustSlot1Configs);
     }
 
     public void startSpinner(double rpm) {
-        m_transferSpinnerMotor.setControl(new VelocityDutyCycle(rpm / 60));
+        m_transferSpinnerMotor.setControl(new VelocityVoltage(rpm / 60));
     }
 
     public void stopSpinner() {
-        m_transferSpinnerMotor.setControl(new VelocityDutyCycle(0));
+        m_transferSpinnerMotor.setControl(new VelocityVoltage(0));
     }
 
     public void startExhaust(double rpm) {
-        m_transferExhaustMotor.setControl(new VelocityDutyCycle(rpm / 60));
+        m_transferExhaustMotor.setControl(new VelocityVoltage(rpm / 60));
     }
 
     public void stopExhaust() {
-        m_transferExhaustMotor.setControl(new VelocityDutyCycle(0));
+        m_transferExhaustMotor.setControl(new VelocityVoltage(0));
     }
 }
