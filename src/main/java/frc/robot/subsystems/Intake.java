f4b4b0f3a7a7378d6755f43fa73a66c074dcaf0b
Saarth <saarth.pandya@gmail.com>
2026-02-02 19:34:14 -0500

    INCOMPLETE - created simStates for deploy and roller

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,11 +4,19 @@ import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import com.ctre.phoenix6.sim.ChassisReference;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 
@@ -25,9 +33,11 @@ public class Intake extends SubsystemBase {
     private final TalonFX m_intakeRollerMotor = new TalonFX(IntakeK.kIntakeRollerCANID);
 
     private MotionMagicVoltage m_MMVRequest = new MotionMagicVoltage(0).withEnableFOC(true).withSlot(0);
-    private VoltageOut m_rollerVoltOut = new VoltageOut(12);
+    private VoltageOut m_rollerVoltOut = new VoltageOut(1);
 
     private VoltageOut zeroingVoltageCtrlReq = new VoltageOut(-0.75); // TODO change this to whatever necessary
+
+    private CommandXboxController m_controller = new CommandXboxController(0);
 
     // TODO possibly uncomment the lines below this if necessary
     // private BooleanSupplier m_currentSpike = () ->
@@ -46,11 +56,40 @@ public class Intake extends SubsystemBase {
     // Logger stuff
     BooleanLogger log_isWorking = WaltLogger.logBoolean(IntakeK.kLogTab, "working");
     DoubleLogger log_getRollerVoltOut = WaltLogger.logDouble(IntakeK.kLogTab, "rollerVoltOut");
+    DoubleLogger log_deployPosition = WaltLogger.logDouble(IntakeK.kLogTab, "deployPos");
+
+    private final DCMotorSim m_deploySim = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(
+            DCMotor.getKrakenX44(1),
+            0.0005,  // Dummy J value
+            1.5 // dummy gearing value
+        ),
+        DCMotor.getKrakenX44(1) // returns gearbox
+    );
+
+    private final DCMotorSim m_rollerSim = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(
+            DCMotor.getKrakenX44(1),
+            0.0005,  // Dummy J value
+            1.5 // dummy gearing value
+        ),
+        DCMotor.getKrakenX44(1) // returns gearbox
+    );
 
     public Intake() {
         m_intakeDeployMotor.getConfigurator().apply(IntakeK.kDeployConfiguration);
         m_intakeRollerMotor.getConfigurator().apply(IntakeK.kIntakeConfiguration);
+        initSim();
+    }
 
+    private void initSim() {
+        var m_deployFXSim = m_intakeDeployMotor.getSimState();
+        m_deployFXSim.Orientation = ChassisReference.CounterClockwise_Positive;
+        m_deployFXSim.setMotorType(TalonFXSimState.MotorType.KrakenX60);
+
+        var m_rollerFXSim = m_intakeDeployMotor.getSimState();
+        m_rollerFXSim.Orientation = ChassisReference.CounterClockwise_Positive;
+        m_rollerFXSim.setMotorType(TalonFXSimState.MotorType.KrakenX44);
     }
 
     public Command spinIntakeRollers() {
@@ -59,9 +98,9 @@ public class Intake extends SubsystemBase {
 
     public Command intakeToAngle(double degs) {
         return runOnce(
-                () -> {
-                    m_intakeDeployMotor.setControl(m_MMVRequest.withPosition(degs));
-                });
+            () -> {
+                m_intakeDeployMotor.setControl(m_MMVRequest.withPosition(degs));
+            });
     }
 
     public Command resetAngle() {
@@ -85,8 +124,12 @@ public class Intake extends SubsystemBase {
     }
 
     public void simulationPeriodic(){
+        // m_controller.a().onTrue(intakeToAngle(0));
+        // m_controller.b().onTrue(intakeToAngle(0.5));
+        // m_controller.x().onTrue(intakeToAngle(1));
         log_isWorking.accept(true);
         spinIntakeRollers();
         log_getRollerVoltOut.accept(m_intakeRollerMotor.getSupplyVoltage().getValueAsDouble());
-        }
+        // log_deployPosition.accept(m_intakeDeployMotor);
+    }
 }
