0e37355e24502b72ec0cfe4c9d4546a4f47f2fd3
alexandra <alexandra.ding.mm@gmail.com>
2026-02-08 12:11:10 -0500

    created several paths and an auton factory

--- src/main/java/frc/robot/Autons/AutonFactory.java
+++ src/main/java/frc/robot/Autons/AutonFactory.java
@@ -19,6 +19,14 @@ public class AutonFactory {
     private final AutoFactory m_autoFactory;
     private final Swerve m_drivetrain;
 
+    //desired pose to go to after fuel pickup
+    private Pose2d postPickupNeutral = new Pose2d(Distance.ofRelativeUnits(6.924767017364502, Meter), 
+        Distance.ofRelativeUnits(2.251265048980713, Meter), new Rotation2d(Math.PI));
+    //desired pose to go to after depot pickup
+    private Pose2d postPickupDepot = new Pose2d(Distance.ofRelativeUnits(1.1576627492904663, Meter), 
+        Distance.ofRelativeUnits(5.958622932434082, Meter), new Rotation2d(0));
+    
+
     public AutonFactory(AutoFactory autoFactory, Swerve drivetrain) {
         m_autoFactory = autoFactory;
         m_drivetrain = drivetrain;
@@ -26,33 +34,19 @@ public class AutonFactory {
     
     /**
      * pickup and shoot one time
-     * simple, hardcoded auton
-     * @return
      */
-    public Command simpleAuton() {
-
-        //can i somehow pull this directly from choreo and not have to do this
-        Pose2d postPickup = new Pose2d(Distance.ofRelativeUnits(6.924767017364502, Meter), 
-            Distance.ofRelativeUnits(2.251265048980713, Meter), new Rotation2d(Math.PI));
-
+    public Command oneNeutralPickup() {
         var allianceOpt = DriverStation.getAlliance();
         if (allianceOpt.isPresent() && allianceOpt.get().equals(Alliance.Red)) {
-            postPickup = AllianceFlipUtil.flip(postPickup);
+            postPickupNeutral = AllianceFlipUtil.flip(postPickupNeutral);
         }
 
         return Commands.sequence(
-            m_autoFactory.resetOdometry("OnePickup"),
-            m_autoFactory.trajectoryCmd("OnePickup"),
+            m_autoFactory.resetOdometry("ToNeutral"),
+            m_autoFactory.trajectoryCmd("ToNeutral"),
 
-            //i feel like there is a better way to do this other than Commands.race()
-            Commands.race(
-                m_drivetrain.swerveToObject(),
-                Commands.waitSeconds(3)
-            ),
-            Commands.race(
-                m_drivetrain.toPose(postPickup),
-                Commands.waitSeconds(2)
-            ),
+            m_drivetrain.swerveToObject().withTimeout(0.5),
+            m_drivetrain.toPose(postPickupNeutral).withTimeout(0.5),
 
             m_autoFactory.resetOdometry("NeutralToShoot"),
             m_autoFactory.trajectoryCmd("NeutralToShoot")
@@ -60,35 +54,87 @@ public class AutonFactory {
     }
 
     /**
+     * pick up two times and shoot once
+     */
+    public Command twoNeutralPickup() {
+        return Commands.sequence(
+            oneNeutralPickup(),
+
+            m_autoFactory.resetOdometry("ShootToNeutral"),
+            m_autoFactory.trajectoryCmd("ShootToNeutral"),
+
+            m_drivetrain.swerveToObject().withTimeout(0.5),
+            m_drivetrain.toPose(postPickupNeutral).withTimeout(.5)
+        );
+    }
+
+    /**
+     * pick up three times and shoot twice
+     */
+    public Command threeNeutralPickup() {
+        return Commands.sequence(
+            twoNeutralPickup(),
+            
+            m_autoFactory.resetOdometry("NeutralToShoot"),
+            m_autoFactory.trajectoryCmd("NeutralToShoot"),
+
+            m_autoFactory.resetOdometry("ShootToNeutral"),
+            m_autoFactory.trajectoryCmd("ShootToNeutral"),
+
+            m_drivetrain.swerveToObject().withTimeout(.5),
+            m_drivetrain.toPose(postPickupNeutral).withTimeout(.5)
+        );
+    }
+
+    /**
+     * pick up four times and shoot thrice
+     */
+    public Command fourNeutralPickup() {
+        return Commands.sequence(
+            threeNeutralPickup(),
+
+            m_autoFactory.resetOdometry("NeutralToShoot"),
+            m_autoFactory.trajectoryCmd("NeutralToShoot"),
+
+            m_autoFactory.resetOdometry("ShootToNeutral"),
+            m_autoFactory.trajectoryCmd("ShootToNeutral"),
+
+            m_drivetrain.swerveToObject().withTimeout(.5),
+            m_drivetrain.toPose(postPickupNeutral).withTimeout(.5)
+        );
+    }
+
+    /**
      * goes to depot once and shoots
-     * another hardcoded auton (´;︵;`)
-     * @return
      */
     public Command oneDepotPickup() {
-        Pose2d postPickup = new Pose2d(Distance.ofRelativeUnits(1.1576627492904663, Meter), 
-            Distance.ofRelativeUnits(5.958622932434082, Meter), new Rotation2d(0));
-
         var allianceOpt = DriverStation.getAlliance();
         if (allianceOpt.isPresent() && allianceOpt.get().equals(Alliance.Red)) {
-            postPickup = AllianceFlipUtil.flip(postPickup);
+            postPickupDepot = AllianceFlipUtil.flip(postPickupDepot);
         }
 
         return Commands.sequence(
             m_autoFactory.resetOdometry("ToDepot"),
             m_autoFactory.trajectoryCmd("ToDepot"),
             
-            Commands.race(
-                m_drivetrain.swerveToObject(),
-                Commands.waitSeconds(3.5)
-            ),
-            Commands.race(
-                m_drivetrain.toPose(postPickup),
-                Commands.waitSeconds(3.5)
-            ),
+            //would we want this for depot?
+            m_drivetrain.swerveToObject().withTimeout(3.5),
+            m_drivetrain.toPose(postPickupDepot).withTimeout(3.5),
 
             m_autoFactory.resetOdometry("DepotToShoot"),
             m_autoFactory.trajectoryCmd("DepotToShoot")
         );
     }
-    
+
+    /**
+     * goes to outpost once, shoots, then goes to neutral to pickup
+     */
+    public Command oneOutpostPickup() {
+        return Commands.sequence(
+            m_autoFactory.resetOdometry("ToOutpost"),
+            m_autoFactory.trajectoryCmd("ToOutpost"),
+            m_autoFactory.resetOdometry("OutpostToNeutral"),
+            m_autoFactory.trajectoryCmd("OutpostToNeutral")
+        );
+    }
 }
