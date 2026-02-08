8177090ed458437f3e678d49609ca7301255d5c9
Saarth <saarth.pandya@gmail.com>
2026-02-08 12:29:43 -0500

    unded whitespace changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,7 +26,7 @@ public class Constants {
     public static class VisionK {
         public static final SimCameraProperties kCamera1SimProps = new SimCameraProperties();
         static {
-            // TODO: [INSERT CAMERA TYPE]
+            //TODO: [INSERT CAMERA TYPE]
             kCamera1SimProps.setCalibration(1920, 1080, Rotation2d.fromDegrees(128.2));
             kCamera1SimProps.setCalibError(0.35, 0.10);
             kCamera1SimProps.setFPS(35);
@@ -36,7 +36,7 @@ public class Constants {
 
         public static final SimCameraProperties kCamera2SimProps = new SimCameraProperties();
         static {
-            // TODO: [INSERT CAMERA TYPE]
+            //TODO: [INSERT CAMERA TYPE]
             kCamera2SimProps.setCalibration(1280, 720, Rotation2d.fromDegrees(100));
             kCamera2SimProps.setCalibError(0.35, 0.10);
             kCamera2SimProps.setFPS(45);
@@ -46,14 +46,14 @@ public class Constants {
 
         public static final String kCamera1CamName = "camera1";
         public static final Transform3d kCamera1CamRoboToCam = new Transform3d(
-                // TODO: Update these numbers
+                //TODO: Update these numbers
                 Units.inchesToMeters(8.238), Units.inchesToMeters(4.81), Units.inchesToMeters(32),
                 new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(40), Units.degreesToRadians(-10)));
         public static final String kCamera1CamSimVisualName = "camera1VisionEstimation";
 
         public static final String kCamera2CamName = "camera2";
         public static final Transform3d kCamera2CamRoboToCam = new Transform3d(
-                // TODO: Update these numbers
+                //TODO: Update these numbers
                 Units.inchesToMeters(9.964), Units.inchesToMeters(-10.499), Units.inchesToMeters(8.442),
                 new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-9.962), Units.degreesToRadians(5)));
         public static final String kCamera2CamSimVisualName = "camera2VisionEstimation";
@@ -84,8 +84,8 @@ public class Constants {
         public static final double kRollersGearing = 1.0/2;
 
         /* IDS */
-        public static final int kDeployCANID = 41; // TODO: change to correct
-        public static final int kRollersCANID = 42; // TODO: change to correct
+        public static final int kDeployCANID = 41; //TODO: change to correct
+        public static final int kRollersCANID = 42; //TODO: change to correct
 
         /* CONFIGS */
         //Deploy Motor
