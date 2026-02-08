6073980dd24be8960ee298c7300a2d0a4a4845b7
Saarth <saarth.pandya@gmail.com>
2026-02-08 12:31:29 -0500

    unded comment multilineage

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -60,9 +60,8 @@ public class Constants {
     }
 
     public static class FieldK {
-        public static final double kFieldLengthMeters = Units.inchesToMeters(651.22); // take with a grain of salt -
-                                                                                      // pulled from field dimensions
-                                                                                      // (welded)
+        // take with a grain of salt - pulled from field dimensions (welded)
+        public static final double kFieldLengthMeters = Units.inchesToMeters(651.22); 
         public static final double kFieldWidthMeters = Units.inchesToMeters(317.69);
 
         public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout
