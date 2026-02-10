63734bd8af074794eb7fa628472b21330b1fdec1
alexandra <alexandra.ding.mm@gmail.com>
2026-02-10 12:52:25 -0500

    fixed auton factory so alliance side can be updated

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,6 +20,8 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import choreo.auto.AutoFactory;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -173,6 +175,11 @@ public class Robot extends TimedRobot {
     }
 
     public Command getAutonomousCommand() {
+        waltAutonFactory.setAlliance( 
+            DriverStation.getAlliance().isPresent() && 
+            DriverStation.getAlliance().get().equals(Alliance.Red)
+        );
+
         return waltAutonFactory.threeNeutralPickup();
     }
 
@@ -211,7 +218,7 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousInit() {
         m_autonomousCommand = getAutonomousCommand();
-
+        
         if (m_autonomousCommand != null) {
             CommandScheduler.getInstance().schedule(m_autonomousCommand);
         }
