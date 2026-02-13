2d1cc11fc722264e5c385c360b166802118804fe
Minh Nguyen <minh.nguyen@waltonrobotics.org>
2026-02-12 20:11:19 -0500

    Add 4 cameras (#10)
    * Added Camera.java. Have not remove unused stuffs.
    
    * Removed unecessary stuff. Added a camera preset for ThirftyCam
    
    * Added comments
    
    * Added comments
    
    * Make sure things compiled.
    
    * Added log_visionSeenPastSecond
    
    * Changed the way how camera are initialize in Constants.java. Changed some parameters in Camera.java.
    
    * Made Code Compiled
    
    * Formating and Changed logging.
    
    * Formatting and change visionSeenLastSec to a class var.
    
    * Setting m_visionSeenLastSecond to currentTime()
    
    * Adjust  Detection.java to the new Cameras
    
    * Remove unused imports.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,17 +29,10 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
-
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import org.photonvision.simulation.SimCameraProperties;
 
 public class Constants {
     public static final boolean kDebugLoggingEnabled = true;
@@ -174,39 +167,43 @@ public class Constants {
     }
 
     public static class VisionK {
-        public static final SimCameraProperties kCamera1SimProps = new SimCameraProperties();
+        public static final Camera[] kCameras = new Camera[4];
+        private static final String kSimCameraSimVisualNames = "VisionEstimation"; //suffixed to each camera name
+
+        //Initialize cameras
         static {
-            //TODO: [INSERT CAMERA TYPE]
-            kCamera1SimProps.setCalibration(1920, 1080, Rotation2d.fromDegrees(128.2));
-            kCamera1SimProps.setCalibError(0.35, 0.10);
-            kCamera1SimProps.setFPS(35);
-            kCamera1SimProps.setAvgLatencyMs(30);
-            kCamera1SimProps.setLatencyStdDevMs(15);
+            kCameras[0] = new Camera(
+                new SimCameraProperties(), 
+                "frontLeftCamera", 
+                kSimCameraSimVisualNames, 
+                Camera.transformToRobo(0, 0, 0, 0, 0, 0) //TODO:
+            );
+            kCameras[0].setProps("ThriftyCam", 0, 0, 0, 0);
+            
+            kCameras[1] = new Camera(
+                new SimCameraProperties(), 
+                "frontRightCamera", 
+                kSimCameraSimVisualNames, 
+                Camera.transformToRobo(0, 0, 0, 0, 0, 0)
+            );
+            kCameras[1].setProps("ThriftyCam", 0, 0, 0, 0);
+
+            kCameras[2] = new Camera(
+                new SimCameraProperties(), 
+                "backLeftCamera", 
+                kSimCameraSimVisualNames, 
+                Camera.transformToRobo(0, 0, 0, 0, 0, 0)
+            );
+            kCameras[2].setProps("ThriftyCam", 0, 0, 0, 0);
+
+            kCameras[3] = new Camera(
+                new SimCameraProperties(), 
+                "backRightCamera", 
+                kSimCameraSimVisualNames, 
+                Camera.transformToRobo(0, 0, 0, 0, 0, 0)
+            );
+            kCameras[3].setProps("ThriftyCam", 0, 0, 0, 0);
         }
-
-        public static final SimCameraProperties kCamera2SimProps = new SimCameraProperties();
-        static {
-            //TODO: [INSERT CAMERA TYPE]
-            kCamera2SimProps.setCalibration(1280, 720, Rotation2d.fromDegrees(100));
-            kCamera2SimProps.setCalibError(0.35, 0.10);
-            kCamera2SimProps.setFPS(45);
-            kCamera2SimProps.setAvgLatencyMs(25);
-            kCamera2SimProps.setLatencyStdDevMs(15);
-        }
-
-        public static final String kCamera1CamName = "camera1";
-        public static final Transform3d kCamera1CamRoboToCam = new Transform3d(
-            //TODO: Update these numbers
-            Units.inchesToMeters(8.238), Units.inchesToMeters(4.81), Units.inchesToMeters(32),
-            new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(40), Units.degreesToRadians(-10)));
-        public static final String kCamera1CamSimVisualName = "camera1VisionEstimation";
-
-        public static final String kCamera2CamName = "camera2";
-        public static final Transform3d kCamera2CamRoboToCam = new Transform3d(
-            //TODO: Update these numbers
-            Units.inchesToMeters(9.964), Units.inchesToMeters(-10.499), Units.inchesToMeters(8.442),
-            new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-9.962), Units.degreesToRadians(5)));
-        public static final String kCamera2CamSimVisualName = "camera2VisionEstimation";
     }
 
     public static class FieldK {
