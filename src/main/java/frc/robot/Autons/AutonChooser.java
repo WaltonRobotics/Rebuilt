a9765c2c1fadd2b675cdf05110b805e89a997595
alexandra <alexandra.ding.mm@gmail.com>
2026-02-14 14:24:38 -0500

    wow it now works! (not the fuel thing thats just exisitng)

--- src/main/java/frc/robot/Autons/AutonChooser.java
+++ src/main/java/frc/robot/Autons/AutonChooser.java
@@ -14,45 +14,37 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class AutonChooser {
     public static SendableChooser<String> m_chooser = new SendableChooser<>();
 
+    //---AUTON PATH NAMES
     public static final String oneRightNeutralPickup = "oneNeutralPickup";
     public static final String twoRightNeutralPickup = "twoNeutralPickup";
     public static final String threeRightNeutralPickup = "threeNeutralPickup";
     public static final String noneSelected = "noAutonSelected";
-    public static final String testAuton = "testAuton";
 
+    //---TOPICS
     public static NetworkTableInstance nte_inst = NetworkTableInstance.getDefault();
     public static NetworkTable nte_autonChooser = nte_inst.getTable("AutonChooser");
 
-    //TOPICS
-    public static BooleanTopic BT_refreshChoice = nte_inst.getBooleanTopic("/AutonChooser/refreshChoice");
+    //---TOPICS
     public static StringTopic ST_autonName = nte_inst.getStringTopic("/AutonChooser/autonName");
-
     public static BooleanTopic BT_autonMade = nte_inst.getBooleanTopic("/AutonChooser/autonMade");
 
-    //PUBLISHERS
-    public static BooleanPublisher pub_refreshChoice;
+    //---PUBLISHERS
     public static StringPublisher pub_autonName;
-
     public static BooleanPublisher pub_autonMade;
 
-    //SUBSCRIBERS
-    public static BooleanSubscriber sub_refreshChoice;
+    //---SUBSCRIBERS
     public static StringSubscriber sub_autonName;
-
     public static BooleanSubscriber sub_autonMade;
 
     public static void initialize() {
-        pub_refreshChoice = BT_refreshChoice.publish();
         pub_autonName = ST_autonName.publish();
 
         pub_autonMade = BT_autonMade.publish();
 
-        sub_refreshChoice = BT_refreshChoice.subscribe(false);
         sub_autonName = ST_autonName.subscribe("No Auton Made");
 
         sub_autonMade = BT_autonMade.subscribe(false);
 
-        pub_refreshChoice.setDefault(false);
         pub_autonName.setDefault("No Auton Made");
 
         pub_autonMade.setDefault(false);
@@ -61,7 +53,6 @@ public class AutonChooser {
         m_chooser.addOption("One Right Neutral Pickup", oneRightNeutralPickup);
         m_chooser.addOption("Two Right Neutral Pickup", twoRightNeutralPickup);
         m_chooser.addOption("Three Right Neutral Pickup ", threeRightNeutralPickup);
-        m_chooser.addOption("Test Auton", testAuton);
 
         SmartDashboard.putData(m_chooser);
     }
