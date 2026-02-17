90d81d5deab044370937dfd4fa73a01fe33854cc
alexandra <alexandra.ding.mm@gmail.com>
2026-02-17 12:21:30 -0500

    cleaned up AutonChooser :D

--- src/main/java/frc/robot/autons/AutonChooser.java
+++ src/main/java/frc/robot/autons/AutonChooser.java
@@ -26,27 +26,32 @@ public class AutonChooser {
     //---TOPICS
     public static StringTopic ST_autonName = nte_inst.getStringTopic("/AutonChooser/autonName");
     public static BooleanTopic BT_autonMade = nte_inst.getBooleanTopic("/AutonChooser/autonMade");
+    public static BooleanTopic BT_makeAuton = nte_inst.getBooleanTopic("/AutonChooser/makeAuton");
 
     //---PUBLISHERS
     public static StringPublisher pub_autonName;
     public static BooleanPublisher pub_autonMade;
+    public static BooleanPublisher pub_makeAuton;
 
     //---SUBSCRIBERS
     public static StringSubscriber sub_autonName;
     public static BooleanSubscriber sub_autonMade;
+    public static BooleanSubscriber sub_makeAuton;
 
     public static void initialize() {
         pub_autonName = ST_autonName.publish();
 
         pub_autonMade = BT_autonMade.publish();
-
-        sub_autonName = ST_autonName.subscribe("No Auton Made");
-
-        sub_autonMade = BT_autonMade.subscribe(false);
+        pub_makeAuton = BT_makeAuton.publish();
 
         pub_autonName.setDefault("No Auton Made");
 
         pub_autonMade.setDefault(false);
+
+        sub_autonName = ST_autonName.subscribe("No Auton Made");
+
+        sub_autonMade = BT_autonMade.subscribe(false);
+        sub_makeAuton = BT_makeAuton.subscribe(false);
 
         m_chooser.setDefaultOption("One Right Neutral Pickup", oneRightNeutralPickup);
         m_chooser.addOption("Two Right Neutral Pickup", twoRightNeutralPickup);
