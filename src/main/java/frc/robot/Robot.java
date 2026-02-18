154733de6e2bd34edbcde99d3a3f00a7a6c97a9e
alexandra <alexandra.ding.mm@gmail.com>
2026-02-18 10:34:26 -0500

    switched bindings to configureBindings()
    note: visualSim is only used in testBindings

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -148,8 +148,8 @@ public class Robot extends TimedRobot {
         .withJoystickReplay();
 
     public Robot() {
-        //configureBindings();
-        configureTestBindings();    //this should be commented out during competition matches
+        configureBindings();
+        //configureTestBindings();    //this should be commented out during competition matches
     }
 
     private Command driveCommand() {
