320d26c0bb8f572b74e378969cb315af081b5c21
PythonBlast7878 <promaker947@gmail.com>
2026-02-18 19:10:03 -0500

    added MotorSim.java javadoc

--- src/main/java/frc/util/MotorSim.java
+++ src/main/java/frc/util/MotorSim.java
@@ -10,13 +10,24 @@ import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import frc.robot.Constants;
 
+/** Motor simulation methods */
 public class MotorSim {
+    /**
+     * Initializes a motor simulation. 
+     * @param motor The motor to simulate.
+     * @param chassisReference The orientation of the motor.
+     * @param motorType The simulated motor type (X44 or X60)
+     */
     public static void initSimFX(TalonFX motor, ChassisReference chassisReference, TalonFXSimState.MotorType motorType) {
         var motorFXSim = motor.getSimState();
         motorFXSim.Orientation = chassisReference;
         motorFXSim.setMotorType(motorType);
     }
-
+    /**
+     * Updates a DC motor simulation
+     * @param motor The motor to simulate.
+     * @param motorSim A DC motor simulation.
+     */
     public static void updateSimFX(TalonFX motor, DCMotorSim motorSim) {
         var motorFXSimState = motor.getSimState();
 
@@ -27,7 +38,11 @@ public class MotorSim {
         motorFXSimState.setRotorVelocity(motorSim.getAngularVelocity().times(motorSim.getGearing()));
         motorFXSimState.setSupplyVoltage(RobotController.getBatteryVoltage());
     }
-
+    /**
+     * Updates a Flywheel motor simulation.
+     * @param motor The motor to simulate.
+     * @param motorSim A simulated flywheel.
+     */
     public static void updateSimFX(TalonFX motor, FlywheelSim motorSim) {
         var motorFXSimState = motor.getSimState();
 
@@ -38,7 +53,12 @@ public class MotorSim {
         motorFXSimState.setSupplyVoltage(RobotController.getBatteryVoltage());
     }
 
-    // designed for the hood servo, but can be used by other servos
+   /**
+     * Updates a servo motor simulation.
+     * This is designed for the hood servo, but it can be used by other servos.
+     * @param servo The servo to simulate.
+     * @param motorSim A DC motor simulation.
+     */
     public static void updateSimServo(Servo servo, DCMotorSim motorSim) {
         double volts = servo.get();
         volts -= 0.5; // sets the range from [0, 1] to be [-0.5, 0.5]
