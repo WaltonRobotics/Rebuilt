9bc388496ed0015b85c4ee1e7b42905ae07397db
alexandra <alexandra.ding.mm@gmail.com>
2026-02-23 19:28:02 -0500

    removed unused imports

--- src/main/java/frc/robot/autons/WaltSimpleAutonFactory.java
+++ src/main/java/frc/robot/autons/WaltSimpleAutonFactory.java
@@ -1,36 +1,20 @@
 package frc.robot.autons;
 
-import java.util.Set;
-import java.util.function.Supplier;
-
 import choreo.auto.AutoFactory;
-import edu.wpi.first.networktables.PubSubOption;
-import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Velocity;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants;
 import frc.robot.Constants.ShooterK;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.Intake.IntakeArmPosition;
-import frc.util.WaltLogger;
-import frc.util.WaltLogger.IntLogger;
 
 public class WaltSimpleAutonFactory {
     private final Superstructure m_superstructre;
-    private final Shooter m_shooter;
     private final AutoFactory m_autoFactory;
 
 
     public WaltSimpleAutonFactory(Superstructure superstructure, Shooter shooter, AutoFactory autoFactory) {
         m_superstructre = superstructure;
-        m_shooter = shooter;
         m_autoFactory = autoFactory;
     }
 
