1e6a1fb69f152d087e93fd73a0d8472e1b34d5a5
alexandra <alexandra.ding.mm@gmail.com>
2026-02-27 21:29:25 -0500

    created right outpost auton, right depot auton, and right neutral zone sweep auton
    TODO: still need to clean up paths (create pose variables) and add depot and outpost autons to chooser

--- src/main/java/frc/robot/autons/WaltSimpleAutonFactory.java
+++ src/main/java/frc/robot/autons/WaltSimpleAutonFactory.java
@@ -4,18 +4,19 @@ import choreo.auto.AutoFactory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.ShooterK;
-import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Superstructure;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Intake.IntakeArmPosition;
 
 public class WaltSimpleAutonFactory {
     private final Superstructure m_superstructre;
     private final AutoFactory m_autoFactory;
+    private final Intake m_intake;
 
-
-    public WaltSimpleAutonFactory(Superstructure superstructure, Shooter shooter, AutoFactory autoFactory) {
+    public WaltSimpleAutonFactory(Superstructure superstructure, AutoFactory autoFactory, Intake intake) {
         m_superstructre = superstructure;
         m_autoFactory = autoFactory;
+        m_intake = intake;
     }
 
     private Command preloadShot() {
@@ -25,27 +26,85 @@ public class WaltSimpleAutonFactory {
         );
     }
 
-    public Command rightOnePickup() {
+    private Command rightOneSweep() {
+        return Commands.parallel(
+            m_autoFactory.trajectoryCmd("RightSweep"),
+            Commands.sequence(
+                m_intake.setIntakeArmPos(IntakeArmPosition.SAFE),
+                Commands.waitSeconds(1),
+                m_superstructre.activateIntake(),
+                Commands.waitSeconds(3),
+                m_superstructre.deactivateIntake(IntakeArmPosition.SAFE),
+                Commands.waitSeconds(1),
+                m_superstructre.activateOuttake(ShooterK.kShooterMaxRPS),
+                Commands.waitSeconds(0.15),
+                m_superstructre.deactivateOuttake()
+            )
+        );
+    }
+
+    public Command rightTwoSweep() {
         return Commands.sequence(
             Commands.parallel(
-                m_autoFactory.resetOdometry("RightPickupOne"),
+                m_autoFactory.resetOdometry("RightSweep"),
                 preloadShot()
             ),
             m_superstructre.deactivateOuttake(),
-            m_autoFactory.trajectoryCmd("RightPickupOne"),
-            m_superstructre.activateIntake().withTimeout(2),
+            rightOneSweep(),
+            m_autoFactory.resetOdometry("RightTurnBack"),
+            m_autoFactory.trajectoryCmd("RightTurnBack"),
+            rightOneSweep()
+        );
+    }
+
+    public Command rightDepotToShoot() {
+        return Commands.sequence(
             Commands.parallel(
-                m_superstructre.deactivateIntake(IntakeArmPosition.SAFE),
-                m_autoFactory.resetOdometry("RightShootOne")
+                m_autoFactory.resetOdometry("RightToDepot"),
+                preloadShot()
             ),
+            m_superstructre.deactivateOuttake(),
             Commands.parallel(
-                m_autoFactory.trajectoryCmd("RightShootOne"),
+                m_autoFactory.trajectoryCmd("RightToDepot"),
                 Commands.sequence(
-                    Commands.waitSeconds(1),  //TODO: tune number
+                    Commands.waitSeconds(1),
+                    m_superstructre.activateIntake()
+                )
+            ),
+            m_autoFactory.resetOdometry("RightDepotToShoot"),
+            m_superstructre.deactivateIntake(IntakeArmPosition.SAFE),
+            Commands.parallel(
+                m_autoFactory.trajectoryCmd("RightDepotToShoot"),
+                Commands.sequence(
+                    Commands.waitSeconds(1),
                     m_superstructre.activateOuttake(ShooterK.kShooterMaxRPS)
                 )
             )
-        ).withName("rightOnePickup");
+            
+        );
+    }
+
+    public Command rightOutpostToShoot() {
+        return Commands.sequence(
+            Commands.parallel(
+                m_autoFactory.resetOdometry("RightToOutpost"),
+                preloadShot()
+            ),
+            m_superstructre.deactivateOuttake(),
+            m_autoFactory.resetOdometry("RightOutpostToNeutral"),
+            Commands.parallel(
+                m_autoFactory.trajectoryCmd("RightOutpostToNeutral"),
+                Commands.sequence(
+                    Commands.waitSeconds(0.5),
+                    m_superstructre.activateOuttake(ShooterK.kShooterMaxRPS),
+                    Commands.waitSeconds(0.5),
+                    m_superstructre.deactivateOuttake(),
+                    m_intake.setIntakeArmPos(IntakeArmPosition.SAFE),
+                    Commands.waitSeconds(0.5),
+                    m_superstructre.activateIntake()
+                )
+            )
+        );
     }
 }
 
