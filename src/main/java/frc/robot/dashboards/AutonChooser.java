a10ae8f9142a95e72ce887e396214be9598f41d7
alexandra <alexandra.ding.mm@gmail.com>
2026-03-23 20:18:04 -0400

    renamed the two 1.5 choreo paths + cleanded up auton factory

--- src/main/java/frc/robot/dashboards/AutonChooser.java
+++ src/main/java/frc/robot/dashboards/AutonChooser.java
@@ -14,14 +14,6 @@ public class AutonChooser {
         m_simpleAutonFactory = simpleAutonFactory;
         m_chooser = new AutoChooser();
 
-        m_chooser.addRoutine("One Right Neutral Pickup",
-            () -> simpleAutonFactory.firstSweep_NoPreload(false));
-        m_chooser.addRoutine("One Left Neutral Pickup",
-            () -> simpleAutonFactory.firstSweep_NoPreload(true));
-        m_chooser.addRoutine("Fast One Right Neutral Pickup",
-            () -> simpleAutonFactory.fastOneSweep(false));
-        m_chooser.addRoutine("Zigzag Two Right Neutral Pickup",
-            () -> simpleAutonFactory.fastRightTwoSweep_ZigZag());
         m_chooser.addRoutine("Reshoot Two Right Neutral Pickup",
             () -> simpleAutonFactory.fastTwoSweep_Reshoot(false));
         m_chooser.addRoutine("Reshoot Two Left Neutral Pickup",
