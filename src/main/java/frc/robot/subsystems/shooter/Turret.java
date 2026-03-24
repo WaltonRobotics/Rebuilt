6db6793b97092ab535056b1ca50cfb1cef440bc3
HrehaanB <hrehaan.bhutada@gmail.com>
2026-03-24 18:57:22 -0400

    remove hominghall and that homing cmd

--- src/main/java/frc/robot/subsystems/shooter/Turret.java
+++ src/main/java/frc/robot/subsystems/shooter/Turret.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems.shooter;
 
 import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.StaticBrake;
-import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -10,14 +8,9 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants;
 
 import static edu.wpi.first.units.Units.*;
@@ -26,8 +19,6 @@ import static frc.robot.Constants.TurretK.kLogTab;
 import static frc.robot.Constants.TurretK.*;
 
 import java.util.function.BooleanSupplier;
-import java.util.function.Consumer;
-
 import frc.util.WaltLogger;
 import frc.util.WaltLogger.BooleanLogger;
 import frc.util.WaltLogger.DoubleLogger;
@@ -38,35 +29,15 @@ public class Turret extends SubsystemBase {
     private boolean m_turretLocked = false;
     private Angle m_turretLockAngle = Degrees.zero();
 
-    public final EventLoop homingEventLoop = new EventLoop();
-
-    private final TalonFX m_turretMotor = new TalonFX(kTurretCANID, Constants.kCanivoreBus); // X44Foc
+    private final TalonFX m_turret = new TalonFX(kTurretCANID, Constants.kCanivoreBus); // X44Foc
     private final PositionVoltage m_PVRequest = new PositionVoltage(0).withEnableFOC(true);
-    private final VoltageOut m_VoltageReq = new VoltageOut(0);
-    private final StaticBrake m_BrakeReq = new StaticBrake();
 
-    // Precomputed: true if turret travel is less than one full rotation (sub-360
-    // cope path)
-    private static final boolean kTurretSubRotation = kTurretMaxRotsFromHome.times(2).magnitude() < 1.0;
-    // Precomputed doubles for hot-path unit conversions
-    private static final double kTurretMinRotsD = kTurretMinRots.in(Rotations);
-    private static final double kTurretMinRotsMagnitudeD = kTurretMinRots.magnitude();
-    private static final double kTurretMaxRotsD = kTurretMaxRots.in(Rotations);
-    private static final double kTurretMaxRotsMagnitudeD = kTurretMaxRots.magnitude();
-
-    private final Command m_homingCommand = turretHomingCmd();
-
-    private final BooleanLogger log_turretHomed = WaltLogger.logBoolean("Shooter/Turret", "Homed");
     // ---LOGIC BOOLEANS
     private boolean m_isTurretHomed = true;
     public BooleanSupplier turretHomedSupp = () -> m_isTurretHomed;
 
     private final CANcoder m_lcmEncA = new CANcoder(19, Constants.kCanivoreBus);
     private final DutyCycleEncoder m_lcmEncB = new DutyCycleEncoder(3);
-
-    private final DigitalInput m_turretHomingHall = new DigitalInput(2);
-    private final Trigger trg_homingHallDirect = new Trigger(homingEventLoop, () -> !m_turretHomingHall.get());
-    private final BooleanLogger log_turretHomingHall = new BooleanLogger(kLogTab, "turretHomeHall");
 
     private final DoubleLogger log_lcmEncAPos = WaltLogger.logDouble(kLogTab, "EncA/Pos");
     private final DoubleLogger log_lcmEncBPos = WaltLogger.logDouble(kLogTab, "EncB/Pos");
@@ -75,28 +46,24 @@ public class Turret extends SubsystemBase {
 
     private final DoubleLogger log_turretControlPos = WaltLogger.logDouble(kLogTab, "turretControlPos");
     private final DoubleLogger log_turretLCMPos = WaltLogger.logDouble(kLogTab, "turretCRTPos");
-    
 
     public Turret() {
-        m_turretMotor.getConfigurator().apply(kTurretTalonFXConfiguration);
+        m_turret.getConfigurator().apply(kTurretTalonFXConfiguration);
+        m_lcmEncA.getConfigurator().apply(kEncoderAConfiguration);
+
         m_lcmEncB.setAssumedFrequency(488);
         m_lcmEncB.setConnectedFrequencyThreshold(400);
 
-        m_lcmEncA.getConfigurator().apply(kEncoderAConfiguration);
+        homeTurret(true);
+    }
 
-        // setDefaultCommand(m_homingCommand);
-
-        double lcmRots = calcTurretAngleLCM(m_lcmEncA.getAbsolutePosition().getValueAsDouble() * 360, -(m_lcmEncB.get() - kEncBOffset) * 360) / 360.0;
-        m_turretMotor.setPosition(lcmRots);
-
-        // m_turretMotor.setPosition(kInitPosition);
-
-        // trg_homingHallDirect.onTrue(Commands.sequence(
-        //         Commands.runOnce(() -> {
-        //             m_homingCommand.cancel();
-        //             WaltLogger.timedPrint("FastHoming OK!!!");
-        //         }),
-        //         Commands.runOnce(() -> homingEventLoop.clear())));
+    private void homeTurret(boolean useLCM) {
+        if (useLCM) {
+            double lcmRots = calcTurretAngleLCM(m_lcmEncA.getAbsolutePosition().getValueAsDouble() * 360, -(m_lcmEncB.get() - kEncBOffset) * 360) / 360.0;
+            m_turret.setPosition(lcmRots);
+        } else {
+            m_turret.setPosition(kInitPosition);
+        }
     }
 
     public void setIntaking(boolean intaking) {
@@ -107,7 +74,7 @@ public class Turret extends SubsystemBase {
     public void setTurretLock(boolean locked) {
         m_turretLocked = locked;
         if (m_turretLocked) {
-            m_turretLockAngle = m_turretMotor.getPosition().getValue();
+            m_turretLockAngle = m_turret.getPosition().getValue();
         }
     }
 
@@ -124,12 +91,12 @@ public class Turret extends SubsystemBase {
     }
 
     public void setTurretPos(Angle rots, AngularVelocity velocityFF) {
-        m_turretMotor.setControl(m_PVRequest.withPosition(rots).withVelocity(velocityFF));
+        m_turret.setControl(m_PVRequest.withPosition(rots).withVelocity(velocityFF));
         log_turretControlPos.accept(rots.in(Rotations));
     }
 
     public void setTurretNeutralMode(NeutralModeValue value) {
-        m_turretMotor.setNeutralMode(value);
+        m_turret.setNeutralMode(value);
     }
 
     // for TestingDashboard
@@ -138,7 +105,7 @@ public class Turret extends SubsystemBase {
     }
 
     public Angle getCurrTurretPos() {
-        return m_turretMotor.getPosition().getValue();
+        return m_turret.getPosition().getValue();
     }
 
     public boolean getTurretLocked() {
@@ -199,39 +166,5 @@ public class Turret extends SubsystemBase {
         }
 
         return bestAngle;
-    }
-
-    public void fastPeriodic() {
-        homingEventLoop.poll();
-    }
-
-    public Command turretHomingCmd() {
-        Runnable init = () -> {
-            WaltLogger.timedPrint("TurretHoming BEGIN");
-            m_turretMotor.setControl(m_VoltageReq.withOutput(kHomingVoltage));
-            m_isTurretHomed = false;
-            log_turretHomed.accept(m_isTurretHomed);
-        };
-
-        Consumer<Boolean> end = (Boolean interrupted) -> {
-            if (interrupted) {
-                m_turretMotor.setControl(m_BrakeReq);
-                log_turretHomed.accept(m_isTurretHomed);
-                WaltLogger.timedPrint("TurretHoming INTERRUPTED!!!");
-                return;
-            }
-
-            m_turretMotor.setPosition(kHomePosition); // Flowkirkentologicalexpialibrostatenuinely
-            m_turretMotor.setControl(m_BrakeReq);
-            removeDefaultCommand();
-            m_isTurretHomed = true;
-            log_turretHomed.accept(m_isTurretHomed);
-        };
-
-        BooleanSupplier isFinished = () -> {
-            return !m_turretHomingHall.get() || m_isTurretHomed;
-        };
-
-        return new FunctionalCommand(init, () ->{}, end, isFinished, this);
     }
 }
