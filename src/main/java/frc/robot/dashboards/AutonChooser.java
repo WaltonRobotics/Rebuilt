c8426252a657779eefc5b75e40f63a92a4da14b1
alexandra <alexandra.ding.mm@gmail.com>
2026-04-01 16:03:21 -0400

    created traj to test adaptable auton factory

--- src/main/java/frc/robot/dashboards/AutonChooser.java
+++ src/main/java/frc/robot/dashboards/AutonChooser.java
@@ -4,15 +4,24 @@ import choreo.auto.AutoChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.AutonK;
+import frc.robot.autons.WaltAdaptableAutonFactory;
 import frc.robot.autons.WaltSimpleAutonFactory;
+import frc.robot.autons.WaltAdaptableAutonFactory.AdaptableAutonInfo;
 
 public class AutonChooser {
     public static AutoChooser m_chooser;
     public static WaltSimpleAutonFactory m_simpleAutonFactory;
+    public static WaltAdaptableAutonFactory m_adaptableAutonFactory;
+    public static AdaptableAutonInfo m_traj1;
+    public static AdaptableAutonInfo m_traj2;
 
     public static void initialize(WaltSimpleAutonFactory simpleAutonFactory) {
         m_simpleAutonFactory = simpleAutonFactory;
         m_chooser = new AutoChooser();
+
+        m_traj1 = new AdaptableAutonInfo("LeftSweepFast", 10, AutonK.kShootingTimeout, false);
+        m_traj2 = new AdaptableAutonInfo("LeftSweepFast_copy1", 10, AutonK.kShootingTimeout, false);
 
         m_chooser.addRoutine("One Right Neutral Pickup",
             () -> simpleAutonFactory.firstSweep_NoPreload(false));
@@ -28,6 +37,8 @@ public class AutonChooser {
             () -> simpleAutonFactory.fastTwoSweep_Reshoot(true));
         m_chooser.addRoutine("Preload Auton",
             () -> simpleAutonFactory.preloadAuton());
+        m_chooser.addRoutine("TEST AUTON",
+            () -> m_adaptableAutonFactory.multiAdaptableAuton("Test Auton", new AdaptableAutonInfo[] {m_traj1, m_traj2}));
 
         SmartDashboard.putData("AutoChooser", m_chooser);
     }
