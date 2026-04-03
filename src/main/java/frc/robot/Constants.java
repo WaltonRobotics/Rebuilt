082a32a616ac74f11fdae41b4eba9e6d73c24000
HrehaanB <hrehaan.bhutada@gmail.com>
2026-04-03 15:39:18 -0400

    new manip barf to unjam rollers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -436,6 +436,7 @@ public class Constants {
         public static final AngularVelocity kIntakeRollersMaxRPS = MotorK.kX60FOCMaxVelocity.div(kIntakeRollersGearing);
         public static final AngularVelocity kIntakeRollersShootRPS = kIntakeRollersMaxRPS.times(0.2);
         public static final AngularVelocity kIntakeRollersShimmyRPS = kIntakeRollersMaxRPS.times(0.2);
+        public static final double kIntakeRollersBarfVolts = -12;
 
         /* IDS */
         public static final int kIntakeArmCANID = 40;
