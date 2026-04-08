2fc7382d8989a47445d23683b54f46f2a2dc842b
alexandra <alexandra.ding.mm@gmail.com>
2026-04-07 23:56:54 -0400

    changed debounce time during auotn

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -117,7 +117,7 @@ public class Constants {
         public static final double kNoPassZoneLeftY = FieldConstants.fieldWidth - 3.2;
         
         public static final double kShooterTimeout = 1.0;
-        public static final double kBallDetectedDebounceTime = 0.9;
+        public static final double kBallDetectedDebounceTime = 1.2; //0.9;
 
         /* MOTOR CONSTANTS */
         public static final double kShooterMoI = 0.000349 * 2.5;  //J for 5 3" 0.53lb flywheels
