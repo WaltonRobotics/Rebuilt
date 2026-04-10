1fd838aebe6be2e57fd89767e0e8a371341cb0b0
HrehaanB <hrehaan.bhutada@gmail.com>
2026-04-10 16:35:55 -0400

    [post-match-52] dynamic dragcoeff if-condition changed

--- src/main/java/frc/robot/subsystems/shooter/ShotCalculator.java
+++ src/main/java/frc/robot/subsystems/shooter/ShotCalculator.java
@@ -507,7 +507,7 @@ public class ShotCalculator {
             // Inline predictTargetPos — no Translation3d/Time allocation
             double coeffDrag = 0.254; //used for SOTM movement
             //2974 RAHHHHHHHHHHHHHHH – correction: more like 254 RAHHHHHHHHHHHHHHH
-            if (vx <= 0.005 || vy <= 0.005) { //NOTE: not sure if these numbers are right
+            if ( (Math.abs(vx) <= 0.05) || (Math.abs(vy) <= 0.05) ) { //NOTE: not sure if these numbers are right
                 coeffDrag = 0.2974; //used during static shot (or when the robot is low speed and should be static shooting)
             }
             // if (distance >= 3.6) {
