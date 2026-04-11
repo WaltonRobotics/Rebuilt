1d907f1502a2780e13ce0142fa0e0a58aa6b31bd
Banks Troutman <btrout.dhrs@gmail.com>
2026-04-10 20:28:22 -0400

    Add GC tracking via polling and JMX notification listener

--- src/main/java/frc/util/PerformanceMonitor.java
+++ src/main/java/frc/util/PerformanceMonitor.java
@@ -1,9 +1,18 @@
 package frc.util;
 
+import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
+import javax.management.Notification;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+import com.sun.management.GcInfo;
 import com.sun.management.OperatingSystemMXBean;
 
 import frc.util.WaltLogger.DoubleLogger;
@@ -18,16 +27,56 @@ public class PerformanceMonitor {
     private final DoubleLogger log_heapMaxMB = WaltLogger.logDouble(kLogTab, "heapMaxMB");
     private final DoubleLogger log_heapCommittedMB = WaltLogger.logDouble(kLogTab, "heapCommittedMB");
     private final DoubleLogger log_nonHeapUsedMB = WaltLogger.logDouble(kLogTab, "nonHeapUsedMB");
+    private final DoubleLogger log_gcCountDelta = WaltLogger.logDouble(kLogTab, "gcCountDelta");
+    private final DoubleLogger log_gcTimeMs = WaltLogger.logDouble(kLogTab, "gcTimeMs");
+    private final DoubleLogger log_gcLastPauseMs = WaltLogger.logDouble(kLogTab, "gcLastPauseMs");
+    private final WaltLogger.StringLogger log_gcLastCause = WaltLogger.logString(kLogTab, "gcLastCause");
 
     private long m_loopStartNanos;
     private final OperatingSystemMXBean m_osBean;
     private final MemoryMXBean m_memBean;
+    private final List<GarbageCollectorMXBean> m_gcBeans;
+    private long m_prevGcCount;
+    private long m_prevGcTimeMs;
+
+    private record GcEvent(String cause, String action, long durationMs) {}
+    private final AtomicReference<GcEvent> m_lastGcEvent = new AtomicReference<>();
 
     private static final double BYTES_TO_MB = 1.0 / (1024.0 * 1024.0);
 
     public PerformanceMonitor() {
         m_osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
         m_memBean = ManagementFactory.getMemoryMXBean();
+        m_gcBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        m_prevGcCount = totalGcCount();
+        m_prevGcTimeMs = totalGcTimeMs();
+
+        NotificationListener gcListener = (Notification notif, Object handback) -> {
+            if (notif.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                var gcNotifInfo = GarbageCollectionNotificationInfo.from(
+                    (javax.management.openmbean.CompositeData) notif.getUserData());
+                GcInfo gcInfo = gcNotifInfo.getGcInfo();
+                m_lastGcEvent.set(new GcEvent(
+                    gcNotifInfo.getGcCause(),
+                    gcNotifInfo.getGcAction(),
+                    gcInfo.getDuration()));
+            }
+        };
+        for (var gc : m_gcBeans) {
+            ((NotificationEmitter) gc).addNotificationListener(gcListener, null, null);
+        }
+    }
+
+    private long totalGcCount() {
+        long total = 0;
+        for (var gc : m_gcBeans) total += gc.getCollectionCount();
+        return total;
+    }
+
+    private long totalGcTimeMs() {
+        long total = 0;
+        for (var gc : m_gcBeans) total += gc.getCollectionTime();
+        return total;
     }
 
     /** Call at the very start of robotPeriodic(). */
@@ -50,5 +99,18 @@ public class PerformanceMonitor {
         log_heapMaxMB.accept(heapMax < 0 ? Double.NaN : heapMax * BYTES_TO_MB);
         log_heapCommittedMB.accept(heap.getCommitted() * BYTES_TO_MB);
         log_nonHeapUsedMB.accept(m_memBean.getNonHeapMemoryUsage().getUsed() * BYTES_TO_MB);
+
+        long gcCount = totalGcCount();
+        long gcTimeMs = totalGcTimeMs();
+        log_gcCountDelta.accept((double) (gcCount - m_prevGcCount));
+        log_gcTimeMs.accept((double) (gcTimeMs - m_prevGcTimeMs));
+        m_prevGcCount = gcCount;
+        m_prevGcTimeMs = gcTimeMs;
+
+        GcEvent evt = m_lastGcEvent.getAndSet(null);
+        if (evt != null) {
+            log_gcLastPauseMs.accept((double) evt.durationMs());
+            log_gcLastCause.accept(evt.action() + ": " + evt.cause());
+        }
     }
 }
